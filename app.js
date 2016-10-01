389cedad70af4df42004aca9ae8275324de7caca
Thomas BARRAS <thomas@MBP-de-Thomas.local>
2016-10-01 13:32:48 -0400

    uncomment

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var sqlite3 = require('sqlite3').verbose();
 var fs = require('fs');
 var db = new sqlite3.Database(':memory:');
 var datas = JSON.parse(fs.readFileSync('datas/datas.json', 'utf8')); // get datas synchronously 
+var queries = require('./methods/queries');
 
 db.serialize(function() {
 
@@ -34,22 +35,18 @@ db.serialize(function() {
 
 	stmt.finalize();     	
 
-	/* SELECT DATAS */
-  /* a)
+  /* SELECT DATAS */
   db.each(  'SELECT num_hotel FROM chambre ' +
             'WHERE (chambre.prix > 50)'
             , function(err, row) {            
     console.log(JSON.stringify(row));
   });
-  */
 
-  /* b)
   db.each(  'SELECT * FROM hotel,chambre ' +
             'WHERE (hotel.num_hotel = chambre.num_hotel)'
             , function(err, row) {            
     console.log(JSON.stringify(row));
   });  
-  */
 
   // TODO
   /* c) 
@@ -58,29 +55,22 @@ db.serialize(function() {
             , function(err, row) {            
     console.log(JSON.stringify(row));
   });    
-  /*
+  */
 
-  /* d)
   db.each("SELECT num_hotel, nom_hotel FROM hotel", function(err, row) {
     console.log(JSON.stringify(row));
   });
-  */
 
-  /* e)
   db.each(  'SELECT num_hotel, num_chambre FROM chambre ' +
             'WHERE (chambre.prix < 50)'
             , function(err, row) {
     console.log(JSON.stringify(row));
   });  
-  */
 
-  /* f)
   db.each(  'SELECT nom_hote, adresse_hote FROM hote', function(err, row) {
     console.log(JSON.stringify(row));
   });  
-  */  
 
-  /* g)
   db.each(  'SELECT prix, type ' +
             'FROM hotel , chambre ' +
             'WHERE ( ' +
@@ -89,17 +79,30 @@ db.serialize(function() {
             , function(err, row) {
     console.log(JSON.stringify(row));
   });   
-  */
 
   db.each(  'SELECT num_hote, nom_hote ' +
             'FROM hotel , reservation, hote ' +
             'WHERE ( ' +
             'hotel.nom_hotel = "hotel Gouverneur" ' +
             'AND hotel.num_hotel = reservation.num_hotel '
-            'AND reservation.num_hote = hote.num_hote )'
+            'AND reservation.num_hote = hote.num_hote ' +
+            'AND reservation.date_debut >= Date.now ' +
+            'AND reservation.date_fin <= Date.now)'
             , function(err, row) {
     console.log(JSON.stringify(row));
   });    
+
+  db.each(  'SELECT num_hote, nom_hote, adresse_hote ' +
+            'FROM hotel , reservation, hote ' +
+            'WHERE ( ' +
+            'hotel.nom_hotel = "hotel Gouverneur" ' +
+            'AND hotel.num_hotel = reservation.num_hotel '
+            'AND reservation.num_hote = hote.num_hote ' +
+            'AND reservation.date_debut >= Date.now ' +
+            'AND reservation.date_fin <= Date.now)'
+            , function(err, row) {
+    console.log(JSON.stringify(row));
+  }); 
 
 
 });           
